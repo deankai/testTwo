85574f6c12e7dcc32e51ad632ef3ee1c5050793c
dean <dean@phoenixnet.com.tw>
2018-06-14 14:46:53 +0800

    Feat Add test

--- TestTwo/ViewController.swift
+++ TestTwo/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    func add (a:Int,b:Int) -> Int {
+        return a + b
     }
 
     override func didReceiveMemoryWarning() {
